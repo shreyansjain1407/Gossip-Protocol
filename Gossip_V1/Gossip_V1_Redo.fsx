43e647a6dab3c23ea9bbdf294c3fbbc18e8740ee
Marcus Elosegui <Marcus.Elosegui@headquarters.polysmith.com>
2021-10-11 16:48:12 -0400

    add failure handling

--- Gossip_V1/Gossip_V1_Redo.fsx
+++ Gossip_V1/Gossip_V1_Redo.fsx
@@ -9,30 +9,37 @@ open Akka.FSharp
 open Akka.Configuration
 
 type Message = 
-    | Initialization of IActorRef []
+    | Initialization of IActorRef [] * int
     | SetNodeCount of int
     | SetStart of int64
     | Rumor of String //For the gossip algorithm, this is the rumor sent
-    | GossipTerminate of String
+    | GossipTerminate of String * int
     | PushSumInit
     | PushSumMsg of Double * Double
     | PushSumTerminate
-    | DeadNeighborAlert
+    | DeadNeighbors of IActorRef
+    | AddNeighbor of IActorRef
+
 
 let stopWatch = System.Diagnostics.Stopwatch()
-
+let system = ActorSystem.Create("System")
 //Keeps the count of all actor related things
 type ProcessController() =
     inherit Actor()
     let mutable terminatedNodes = 0
     let mutable start = 0L //This is the starting time
     let mutable totalNodes = 0
+    let mutable nodeSet = Set.empty
+    let mutable currentNodes = 0
 
     override x.OnReceive(receivedMsg) =
         match receivedMsg :?> Message with
-            | GossipTerminate msg ->
+            | GossipTerminate (msg, node) ->
                 let curTime = stopWatch.ElapsedMilliseconds //This is the time at which the message was received
                 terminatedNodes <- terminatedNodes + 1 //Incrementing Messages received
+                currentNodes <- currentNodes - 1
+                let tempSet = Set.empty.Add(node)
+                nodeSet <- Set.difference nodeSet tempSet
                 if terminatedNodes = totalNodes then
                     stopWatch.Stop()
                     printfn "Gossip:\nStartTime: %i, FinishTime: %i, Difference: %i" start curTime (curTime - start)
@@ -40,6 +47,8 @@ type ProcessController() =
             
             |SetNodeCount count ->
                 totalNodes <- count
+                currentNodes <- count
+                nodeSet <- Set.ofSeq [1 .. totalNodes]
 
             |SetStart time ->
                 start <- time
@@ -47,11 +56,20 @@ type ProcessController() =
             |PushSumTerminate ->
                 let curTime = stopWatch.ElapsedMilliseconds
                 terminatedNodes <- terminatedNodes + 1
+                currentNodes <- currentNodes - 1
                 if terminatedNodes = totalNodes then
                     stopWatch.Stop()
                     printfn "PushSum:\n StartTime: %i, FinishTime: %i, Difference: %i" start curTime (curTime - start)
                     Environment.Exit(0)
+            | DeadNeighbors node ->
+                let rand = new Random()
+                let setArray =  Set.toArray nodeSet
+                let newNeighbor = setArray.[rand.Next currentNodes]
+                let loneNode = "akka://system/user/ProcessController" + string node
+                let newNode = "akka://system/user/ProcessController" + string newNeighbor
 
+                //select loneNode system <! AddNeighbor(newNeighbor)
+                select newNode system <! AddNeighbor(node)
             | _ -> ()
 
 //This is the main actor that will be transmitting all the "Good Stuff"
@@ -67,18 +85,21 @@ type Node(processController: IActorRef, msg: int, designatedNum: int) =
     let rLimit = 3
     let tLimit = 10
     let pushSumLimit = (10. ** -10.)
+    let mutable node = 0
 
     override x.OnReceive(nodeMsg) =
         match nodeMsg :?> Message with
-        |Initialization neighbourArr ->
+        |Initialization (neighbourArr, nodeNum) ->
             neighbour <- neighbourArr
-
+            node <- nodeNum
         |Rumor str ->
             //Here the rumor is received by the actor and forwarded
             msgCount <- msgCount + 1
             if(msgCount = tLimit) then
                 //Notifying the process controller that an actor has reached it's limit
-                processController <! GossipTerminate("I have lived long enough, but you must continue")
+                processController <! GossipTerminate("I have lived long enough, but you must continue", node)
+            if(neighbour.Length = 0) then
+                processController <! DeadNeighbors(Actor.Context.Self)
             if (msgCount < (tLimit * neighbour.Length)) then
                 neighbour.[System.Random().Next(0, neighbour.Length)] <! Rumor(str)
         
@@ -109,6 +130,10 @@ type Node(processController: IActorRef, msg: int, designatedNum: int) =
                 s <- s/2.
                 w <- w/2.
                 neighbour.[System.Random().Next(0,neighbour.Length)] <! PushSumMsg(s,w)
+        | AddNeighbor node ->
+            let newNode = [|node|]
+            //let newNode : IActorRef =  (select ("akka://system/user/ProccessController" + string node) system)  <! Initialization
+            neighbour <- Array.append neighbour newNode  
             
         | _ -> ()
         
@@ -118,9 +143,9 @@ let mutable nodeCount = int (string (fsi.CommandLineArgs.GetValue 1))
 let topology = string (fsi.CommandLineArgs.GetValue 2)
 let algo = string (fsi.CommandLineArgs.GetValue 3)
 let tempx = float nodeCount
-let system = ActorSystem.Create("System")
+
 nodeCount = 
-    if topology = "3D" || topology = "imp3D" then
+    if topology = "2D" || topology = "imp2D" then
         int (floor((tempx ** 0.5) ** 2.0))
     else
         int tempx
@@ -140,7 +165,7 @@ match topology with
         actorArray.[i] <- system.ActorOf(Props.Create(typeof<Node>, processController, 10, i+1), "ProcessController" + string i)
     //Loop to initialize the neighbours of spawned actors in this case all are neighnours
     for i in [0 .. nodeCount] do
-        actorArray.[i] <! Initialization(actorArray)
+        actorArray.[i] <! Initialization(actorArray, i)
     
     let baseActor = Random().Next(0, nodeCount)
     if algo = "gossip" then
@@ -157,29 +182,13 @@ match topology with
         processController <! SetStart(stopWatch.ElapsedMilliseconds)
         actorArray.[baseActor] <! PushSumInit
 
-| "3D" ->
-    printfn "3D topology"
-    let gridSide = int (ceil (sqrt (float nodeCount)))
-    printfn "GridSize: %i" gridSide
-    printfn "Number of Nodes: %i" (gridSide*gridSide)
-    let actorArray = Array.zeroCreate(gridSide * gridSide)
+| "2D" ->
+    let actorArray = Array.zeroCreate( nodeCount + 1)
     //Loop to spawn actors
-    for i in [0 .. gridSide * gridSide - 1] do
-        actorArray.[i] <- system.ActorOf(Props.Create(typeof<Node>, processController, 10, i+1), "ProcessController" + string i)
+    for i in [0 .. nodeCount] do
+        actorArray.[i] <- system.ActorOf(Props.Create(typeof<Node>, processController, 10, i+1), "ProcessController")
     //Loop to initialize the neighbours of spawned actors in this case all are neighnours
-    for i in [0 .. gridSide - 1] do
-        for j in [0 .. gridSide - 1] do
-            let mutable neighbourArr: IActorRef [] = [||]
-            if i > 0 then
-                neighbourArr <- Array.append neighbourArr[|actorArray.[(i-1) * gridSide + j]|]
-            if j < gridSide - 1 then
-                neighbourArr <- Array.append neighbourArr[|actorArray.[(i * gridSide) + j + 1]|]
-            if i < gridSide - 1 then
-                neighbourArr <- Array.append neighbourArr[|actorArray.[(i+1) * gridSide + j]|]
-            if j > 0 then
-                neighbourArr <- Array.append neighbourArr[|actorArray.[(i * gridSide) + j - 1]|]
-            
-            actorArray.[i*gridSide + 1] <! Initialization(neighbourArr)
+
 
     let baseActor = Random().Next(0, nodeCount)
     if algo = "gossip" then
@@ -197,20 +206,12 @@ match topology with
         actorArray.[baseActor] <! PushSumInit
     
 | "Line" ->
-    printfn "Line topology"
     let actorArray = Array.zeroCreate( nodeCount + 1)
     //Loop to spawn actors
     for i in [0 .. nodeCount] do
-        actorArray.[i] <- system.ActorOf(Props.Create(typeof<Node>, processController, 10, i+1), "ProcessController" + string i)
+        actorArray.[i] <- system.ActorOf(Props.Create(typeof<Node>, processController, 10, i+1), "ProcessController")
     //Loop to initialize the neighbours of spawned actors in this case all are neighnours
-    for i in [0 .. nodeCount] do
-        let mutable neighbourArr: IActorRef [] = [||]
-        if i > 0 then
-            neighbourArr <- Array.append neighbourArr[|actorArray.[i - 1]|]
-        if i < nodeCount - 1 then
-            neighbourArr <- Array.append neighbourArr[|actorArray.[i + 1]|]
-            
-        actorArray.[i] <! Initialization(actorArray)
+    
     
     let baseActor = Random().Next(0, nodeCount)
     if algo = "gossip" then
@@ -227,30 +228,14 @@ match topology with
         processController <! SetStart(stopWatch.ElapsedMilliseconds)
         actorArray.[baseActor] <! PushSumInit
     
-| "imp3D" ->
+| "imp2D" ->
     let actorArray = Array.zeroCreate( nodeCount + 1)
     //Loop to spawn actors
     for i in [0 .. nodeCount] do
-        actorArray.[i] <- system.ActorOf(Props.Create(typeof<Node>, processController, 10, i+1), "ProcessController" + string i)
-    //Loop to initialize the neighbours of spawned actors in this case all are neighnours     
-
-    let mutable i = 0
-    while i < nodeCount do
-        let mutable neighborArr: IActorRef [] = [||]
-        for j in [0 .. 5] do
-            if i + j > nodeCount then
-                //do nothing
-                neighborArr <- neighborArr
-            else
-                neighborArr <- Array.append neighborArr[|actorArray.[i+j]|]
-            if i = 0 && j = 5 then
-                neighborArr <- Array.append neighborArr[|actorArray.[i+j+1]|]
-            if j = 5 then
-                i <- i + j
-                if i = 0 then
-                    i <- i + 1
-        actorArray.[i] <! Initialization(neighborArr)
-           
+        actorArray.[i] <- system.ActorOf(Props.Create(typeof<Node>, processController, 10, i+1), "ProcessController")
+    //Loop to initialize the neighbours of spawned actors in this case all are neighnours
+    
+    
     let baseActor = Random().Next(0, nodeCount)
     if algo = "gossip" then
         //Whenever an algorithm is started, we shall notify the process controller about
